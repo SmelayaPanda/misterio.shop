a263a1bf7ca1107db79dc8bb847c87f3474ca459
panda <SmelayaPanda@mail.ru>
2018-03-28 23:03:58 +0700

    live chat icon

--- src/components/home/HomeFrame.vue
+++ src/components/home/HomeFrame.vue
@@ -7,6 +7,12 @@
     <div id="flip_down_wrapper">
       <p id="flip_down">Листай вниз</p>
     </div>
+    <!--LIVE CHAT-->
+    <div id="load_wrapper">
+      <div class="load">
+        <i class="fa fa-heartbeat fa-3x"></i>
+      </div>
+    </div>
     <slot name="bg_image"></slot>
     <slot name="content"></slot>
   </div>
@@ -62,5 +68,239 @@ export default {
     left: 40px;
     font-size: 19px;
     color: $color-secondary;
+  }
+
+  /* the loader */
+  /* the loader */
+  #load_wrapper {
+    position: absolute;
+    bottom: 40px;
+    right: 40px;
+  }
+  .load {
+    width: 80px;
+    height: 80px;
+    /** height is required as absolute value **/
+    background-color: $color-secondary;
+    border-radius: 100%;
+    position: relative;
+    animation: pulse 2000ms linear infinite;
+    -webkit-animation: pulse 2000ms linear infinite;
+    -moz-animation: pulse 2000ms linear infinite;
+  }
+
+  .load i {
+    position: absolute;
+    top: 22px;
+    left: 18px;
+    color: white;
+    text-shadow: 1px 1px 2px black, 0 0 1em red;
+  }
+
+  .load:after,
+  .load:before {
+    display: block;
+    margin: auto;
+    position: absolute;
+    content: "";
+    width: 80px;
+    height: 80px;
+    border-radius: 100%;
+    background-color: $color-secondary;
+  }
+
+  .load:after {
+    z-index: -100;
+    -webkit-animation: outer-ripple 2000ms linear infinite;
+    -moz-animation: outer-ripple 2000ms linear infinite;
+    animation: outer-ripple 2000ms linear infinite;
+  }
+
+  .load:before {
+    z-index: -200;
+    -webkit-animation: inner-ripple 2000ms linear infinite;
+    -moz-animation: inner-ripple 2000ms linear infinite;
+    animation: inner-ripple 2000ms linear infinite;
+  }
+
+  /* outer ripple */
+
+  @keyframes pulse {
+    0% {
+      transform: scale(0.8);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    10% {
+      transform: scale(1.1);
+      filter: alpha(opacity=1);
+      opacity: 1;
+    }
+    20% {
+      transform: scale(0.9);
+      filter: alpha(opacity=1);
+      opacity: 1;
+    }
+    100% {
+      transform: scale(0.8);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+  }
+
+  @-moz-keyframes pulse {
+    0% {
+      transform: scale(0.8);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    10% {
+      transform: scale(1.1);
+      filter: alpha(opacity=1);
+      opacity: 1;
+    }
+    20% {
+      transform: scale(0.9);
+      filter: alpha(opacity=1);
+      opacity: 1;
+    }
+    100% {
+      transform: scale(0.8);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+  }
+
+  @-webkit-keyframes pulse {
+    0% {
+      transform: scale(0.8);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    10% {
+      transform: scale(1.1);
+      filter: alpha(opacity=1);
+      opacity: 1;
+    }
+    20% {
+      transform: scale(0.9);
+      filter: alpha(opacity=1);
+      opacity: 1;
+    }
+    100% {
+      transform: scale(0.8);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+  }
+
+  @keyframes outer-ripple {
+    0% {
+      transform: scale(1);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    80% {
+      transform: scale(3);
+      filter: alpha(opacity=0);
+      opacity: 0;
+    }
+    100% {
+      transform: scale(3);
+      filter: alpha(opacity=0);
+      opacity: 0;
+    }
+  }
+
+  @-webkit-keyframes outer-ripple {
+    0% {
+      transform: scale(1);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    80% {
+      transform: scale(3);
+      filter: alpha(opacity=0);
+      opacity: 0;
+    }
+    100% {
+      transform: scale(3);
+      filter: alpha(opacity=0);
+      opacity: 0;
+    }
+  }
+
+  @-moz-keyframes outer-ripple {
+    0% {
+      transform: scale(1);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    80% {
+      transform: scale(3);
+      filter: alpha(opacity=0);
+      opacity: 0;
+    }
+    100% {
+      transform: scale(3);
+      filter: alpha(opacity=0);
+      opacity: 0;
+    }
+  }
+
+  /* inner ripple */
+
+  @keyframes inner-ripple {
+    0% {
+      transform: scale(1);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    30% {
+      transform: scale(1);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    100% {
+      transform: scale(2.5);
+      filter: alpha(opacity=0);
+      opacity: 0;
+    }
+  }
+
+  @-webkit-keyframes inner-ripple {
+    0% {
+      transform: scale(1);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    30% {
+      transform: scale(1);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    100% {
+      transform: scale(2.5);
+      filter: alpha(opacity=0);
+      opacity: 0;
+    }
+  }
+
+  @-moz-keyframes inner-ripple {
+    0% {
+      transform: scale(1);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    30% {
+      transform: scale(1);
+      filter: alpha(opacity=50);
+      opacity: 0.5;
+    }
+    100% {
+      transform: scale(2.5);
+      filter: alpha(opacity=0);
+      opacity: 0;
+    }
   }
 </style>
