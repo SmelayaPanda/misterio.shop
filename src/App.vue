bf8140cbc3bc27931a405506345ec17096a3d8ac
panda <SmelayaPanda@mail.ru>
2018-03-28 12:31:49 +0700

    fonts

--- src/App.vue
+++ src/App.vue
@@ -23,13 +23,19 @@ export default {
 </script>
 
 <style lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,500,700&subset=cyrillic,cyrillic-ext');
+  @import url('https://fonts.googleapis.com/css?family=Yeseva+One&subset=cyrillic,cyrillic-ext');
+
+  $primary-font: 'Montserrat', sans-serif;
+  $secondary-font: 'Yeseva One', cursive;
+
   body, html {
     margin: 0;
     padding: 0;
   }
 
   #app {
-    font-family: Montserrat, 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: $primary-font;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     background-color: white;
