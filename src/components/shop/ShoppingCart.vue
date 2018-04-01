58e814394cede2d777f4da1902c7687f8cc0d106
panda <SmelayaPanda@mail.ru>
2018-04-01 12:08:24 +0700

    cart header

--- src/components/shop/ShoppingCart.vue
+++ src/components/shop/ShoppingCart.vue
@@ -20,7 +20,35 @@
             </p>
           </router-link>
           <!--PRODUCTS-->
-          <v-divider class="secondary mt-3 mb-3"></v-divider>
+          <!--TODO: mobile version of cart-->
+          <el-row
+            v-if="userCart.length !== 0 &&
+                  this.$vuetify.breakpoint.name !== 'xs' &&
+                  this.$vuetify.breakpoint.name !== 'sm'"
+            id="cart_table_header"
+            type="flex"
+            justify="center"
+            style="flex-wrap: wrap; align-items: center;">
+            <el-col :span="3" align="left">
+              <span>ТОВАР</span>
+            </el-col>
+            <el-col :span="8" align="left">
+              <span>ОПИСАНИЕ</span>
+            </el-col>
+            <el-col :span="4" align="center">
+              <span>КОЛИЧЕСТВО</span>
+            </el-col>
+            <el-col :span="3" align="center" class="pr-2">
+              <span>СУММА</span>
+            </el-col>
+            <el-col :span="2" align="center">
+              <span>УДАЛИТЬ</span>
+            </el-col>
+            <el-col :span="3" align="center">
+              <span></span>
+            </el-col>
+          </el-row>
+          <v-divider class="secondary mb-3"></v-divider>
           <div
             v-for="product in userCart"
             :key="product.productId"
@@ -29,7 +57,7 @@
               type="flex"
               justify="center"
               style="flex-wrap: wrap; align-items: center">
-              <el-col :sm="3" :md="3" :lg="3" :xl="3" align="left">
+              <el-col :xs="24" :sm="3" :md="3" :lg="3" :xl="3" align="left">
                 <img
                   v-if="product.img_0.thumbnail"
                   :src="product.img_0.thumbnail"
@@ -46,7 +74,7 @@
                 </span>
                 </router-link>
               </el-col>
-              <el-col :xs="24" :sm="4" :md="4" :lg="4" :xl="4" align="left">
+              <el-col :xs="24" :sm="4" :md="4" :lg="4" :xl="4" align="center">
                 <div>
                   <i @click="product.qty--" class="el-icon-arrow-left count_control"></i>
                   <span class="product_count">
@@ -246,5 +274,12 @@ export default {
     width: 30px;
     height: 30px;
     border: 1px solid $color-secondary;
+  }
+
+  #cart_table_header {
+    font-size: 12px;
+    color: $color-info;
+    margin-top: 30px;
+    margin-bottom: 7px;
   }
 </style>
