e8cf0f07a8d25a87a283741166aa6a4c70ed2ea9
panda <SmelayaPanda@mail.ru>
2018-04-07 18:13:45 +0700

    dump data

---
+++ src/components/admin/developer/Developer.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>Developer board</h1>
+    <el-button @click="getData" class="success_a white--text">
+      Dump data
+    </el-button>
+  </div>
+</template>
+
+<script>
+import * as firebase from 'firebase'
+import JSZip from 'jszip'
+import FileSaver from 'file-saver'
+
+export default {
+  name: 'Developer',
+  data () {
+    return {
+      collections: [
+        'products',
+        'orders',
+        'oneclick'
+      ]
+    }
+  },
+  methods: {
+    getData () {
+      let zip = new JSZip()
+      this.$store.dispatch('LOADING', true)
+
+      let getCollectionData = function (name) {
+        return firebase.firestore().collection(name).get()
+          .then(snap => {
+            let data = {}
+            snap.docs.forEach(el => {
+              data[el.id] = el.data()
+            })
+            zip.file(name + '.json', JSON.stringify(data))
+          })
+      }
+      let actions = []
+      this.collections.forEach(name => actions.push(getCollectionData(name)))
+
+      Promise.all(actions)
+        .then(() => {
+          zip.generateAsync({type: 'blob'})
+            .then((content) => {
+              let date = new Date().toISOString().substring(0, 10)
+              FileSaver.saveAs(content, 'dump_' + date + '.zip')
+              this.$store.dispatch('LOADING', false)
+            })
+            .catch(err => {
+              console.log(err)
+              this.$store.dispatch('LOADING', false)
+            })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  #dump_data {
+    margin-top: 15px;
+    color: $color-primary_a;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,7 @@ import Account from '@/components/account/Account'
 // import AuthGuard from '@/router/auth-guard'
 // ADMIN
 import AdminPanel from '@/components/admin/AdminPanel'
+import Developer from '@/components/admin/developer/Developer'
 import Dashboard from '@/components/admin/dashboard/Dashboard'
 import AdminProducts from '@/components/admin/products/AdminProducts'
 import AdminOrders from '@/components/admin/orders/AdminOrders'
@@ -104,6 +105,10 @@ export default new Router({
         {
           path: '',
           component: Dashboard
+        },
+        {
+          path: 'developer',
+          component: Developer
         },
         {
           path: 'products',
