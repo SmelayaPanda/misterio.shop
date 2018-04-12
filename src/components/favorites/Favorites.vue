0903719cf34e70cfadd02cc7caefd5657123c240
panda <SmelayaPanda@mail.ru>
2018-04-12 21:28:10 +0700

    favorites

--- src/components/favorites/Favorites.vue
+++ src/components/favorites/Favorites.vue
@@ -16,7 +16,7 @@
       justify="center"
       style="flex-wrap: wrap">
       <el-col :xs="23" :sm="23" :md="18" :lg="18" :xl="15" align="left">
-        <el-row type="flex" justify="center" style="flex-wrap: wrap">
+        <el-row v-if="Object.keys(favorites).length" type="flex" justify="center" style="flex-wrap: wrap">
           <el-col
             v-for="product in favorites"
             :key="product.productId"
@@ -24,6 +24,17 @@
             align="left">
             <favorite-product :id="product.productId"/>
           </el-col>
+        </el-row>
+        <el-row v-else type="flex" justify="center" style="flex-wrap: wrap">
+          <el-col :span="24">
+          <p id="favorite_list" align="center">Список Ваших желаний пуст</p> <br>
+          </el-col>
+          <router-link to="/shop">
+            <p id="into_catalog">
+              В каталог
+              <v-icon class="secondary--text">arrow_forward</v-icon>
+            </p>
+          </router-link>
         </el-row>
       </el-col>
     </el-row>
@@ -60,4 +71,13 @@ export default {
     margin-right: 3px;
   }
 
+  #into_catalog {
+    color: $color-secondary;
+    font-size: 16px;
+    font-weight: 600;
+  }
+  #favorite_list {
+    color: white;
+    margin-bottom: 0;
+  }
 </style>
