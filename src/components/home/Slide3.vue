800aa44fae65689761cbba483a6b3c8ac521b04a
panda <SmelayaPanda@mail.ru>
2018-04-22 21:50:15 +0700

    slide 3 mobile controls

--- src/components/home/Slide3.vue
+++ src/components/home/Slide3.vue
@@ -2,41 +2,49 @@
   <div>
     <white-pattern color="black"></white-pattern>
     <el-row type="flex" justify="center" style="flex-wrap: wrap">
-      <el-col :span="22">
+      <el-col :xs="22" :sm="20" :md="22" :lg="22" :xl="22">
         <el-row type="flex" justify="center" style="flex-wrap: wrap">
           <transition name="app-fade-left">
-            <el-col v-if="isLoadedSlide3" id="slide_3_left" :xs="24" :sm="22" :md="10" :lg="10" :xl="8">
-              <div v-for="sale in sales"
-                   :key="sale.id"
-                   v-if="Object.values(sales).indexOf(sale) === curSlide">
-                <p id="title_3_main" align="left">
-                  <span class="dark_title">
-                    {{ sale.title | snippet(100)}}
-                  </span>
-                  <!--<span class="dark_title"> ПРИ ПОКУПКЕ </span>-->
-                  <!--<span class="white_title"> 2Х </span> <br>-->
-                  <!--<span class="dark_title"> ВТУЛОК </span>-->
-                  <!--<span class="white_title"> 3УЮ </span> <br>-->
-                  <!--<span class="dark_title"> ЗАБТРАЕТЕ </span> <br>-->
-                  <!--<span class="white_title"> В ПОДАРОК! </span>-->
-                </p>
-                <p v-if="sale.description"
-                   v-html="sale.description.slice(0, 300)"
-                   id="title_3_sub" align="left"></p>
-                <div align="left">
-                  <router-link to="/news" exact>
-                    <el-button id="all_sales_btn">
-                      Все акции
-                    </el-button>
-                  </router-link>
+            <el-col v-if="isLoadedSlide3"
+                    id="sale_descr_col"
+                    :xs="24" :sm="22" :md="10" :lg="10" :xl="8">
+              <transition name="fade">
+                <div v-for="sale in sales"
+                     :key="sale.id"
+                     v-if="Object.values(sales).indexOf(sale) === curSlide">
+                  <div id="sale_descr_wrap">
+                    <p id="sale_title">{{ sale.title | snippet(100)}}</p>
+                    <p v-if="sale.description"
+                       v-html="sale.description.slice(0, 300)"
+                       id="sale_subtitle"></p>
+                    <div align="left">
+                      <router-link to="/news" exact>
+                        <el-button id="all_sales_btn">
+                          Все акции
+                        </el-button>
+                      </router-link>
+                    </div>
+                  </div>
+                  <el-row id="mobile_controls">
+                    <el-col :span="12">
+                      <p class="sales_swiper_forward" @click="nextSale">
+                        Вперед
+                      </p>
+                    </el-col>
+                    <el-col :span="12">
+                      <p class="sales_swiper_back" @click="prevSale">
+                        Назад
+                      </p>
+                    </el-col>
+                  </el-row>
                 </div>
-              </div>
+              </transition>
             </el-col>
           </transition>
           <transition name="app-fade-right">
             <el-col v-show="isLoadedSlide3"
                     id="sales_swiper"
-                    :md="12" :lg="11" :xl="11" align="right">
+                    :md="12" :lg="11" :xl="11">
               <swiper
                 @slideChange="updateCurIndex"
                 ref="salesSwiper"
@@ -44,18 +52,18 @@
                 <swiper-slide
                   v-for="sale in sales"
                   :key="sale.id">
-                  <img v-if="sale.img_0" :src="sale.img_0.original" alt="">
+                  <img v-if="sale.img_0" :src="sale.img_0.original">
                 </swiper-slide>
                 <div class="swiper-pagination" slot="pagination"></div>
               </swiper>
-              <el-row>
+              <el-row id="desctop_controls">
                 <el-col :span="12">
-                  <p id="sales_swiper_forward" @click="salesSwiper.slideNext()">
+                  <p class="sales_swiper_forward" @click="salesSwiper.slideNext()">
                     Вперед
                   </p>
                 </el-col>
                 <el-col :span="12">
-                  <p id="sales_swiper_back" @click="salesSwiper.slidePrev()">
+                  <p class="sales_swiper_back" @click="salesSwiper.slidePrev()">
                     Назад
                   </p>
                 </el-col>
@@ -82,11 +90,12 @@ export default {
       swiperOption: {
         effect: 'cube',
         grabCursor: true,
+        loop: true,
+        speed: 700,
         cubeEffect: {
           shadow: true,
           slideShadows: true,
           shadowOffset: 20,
-          speed: 1000,
           shadowScale: 0.94
         },
         pagination: {
@@ -101,6 +110,20 @@ export default {
     },
     updateCurIndex () {
       this.curSlide = this.salesSwiper.realIndex
+    },
+    nextSale () {
+      if (this.curSlide === this.sales.length - 1) {
+        this.curSlide = 1
+      } else {
+        this.curSlide++
+      }
+    },
+    prevSale () {
+      if (this.curSlide === 0) {
+        this.curSlide = this.sales.length - 1
+      } else {
+        this.curSlide--
+      }
     }
   },
   computed: {
@@ -132,16 +155,24 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  #slide_3_left {
+  #sale_descr_col {
     padding-left: 40px;
     margin-top: 17vh;
   }
 
-  #title_3_main {
-    padding-right: 20px;
+  #sale_descr_wrap {
   }
 
-  #title_3_main:after {
+  #sale_title {
+    font-family: $secondary-font;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: left;
+    padding-right: 20px;
+    color: $color-primary;
+  }
+
+  #sale_title:after {
     content: "";
     display: block;
     width: 45%;
@@ -149,26 +180,11 @@ export default {
     border-bottom: 1px solid $color-primary;
   }
 
-  #title_3_sub {
+  #sale_subtitle {
     color: white;
     font-size: 12px;
     padding-right: 30px;
-  }
-
-  @mixin slide_3_title {
-    font-family: $secondary-font;
-    font-size: 32px;
-    font-weight: 500;
-  }
-
-  .dark_title {
-    @include slide_3_title;
-    color: $color-primary;
-  }
-
-  .white_title {
-    @include slide_3_title;
-    color: white;
+    text-align: left;
   }
 
   #all_sales_btn {
@@ -195,8 +211,8 @@ export default {
   }
 
   /* controls */
-
-  #sales_swiper_forward, #sales_swiper_back {
+  .sales_swiper_forward,
+  .sales_swiper_back {
     font-size: 12px;
     font-weight: 500;
     color: white;
@@ -204,17 +220,17 @@ export default {
     transition: all .5s;
   }
 
-  #sales_swiper_forward:hover,
-  #sales_swiper_back:hover {
+  .sales_swiper_forward:hover,
+  .sales_swiper_back:hover {
     cursor: pointer;
     text-shadow: $secondary-text-shadow
   }
 
-  #sales_swiper_forward {
+  .sales_swiper_forward {
     text-align: left;
   }
 
-  #sales_swiper_back {
+  .sales_swiper_back {
     text-align: right;
   }
 
@@ -227,13 +243,17 @@ export default {
     border-top: 2px solid $color-primary;
   }
 
-  #sales_swiper_forward:before {
+  .sales_swiper_forward:before {
     @include control_line;
   }
 
-  #sales_swiper_back:before {
+  .sales_swiper_back:before {
     @include control_line;
     right: 0;
+  }
+
+  #mobile_controls {
+    display: none;
   }
 
   /* MEDIA */
@@ -241,17 +261,19 @@ export default {
     #sales_swiper {
       display: none;
     }
+    #mobile_controls {
+      display: block;
+    }
   }
 
   @media only screen and (max-width: $xs-screen) {
-    #slide_3_left {
+    #sale_descr_col {
       margin-top: 8vh;
+      padding-left: 10px;
     }
 
-    #title_3_main {
+    #sale_title {
       margin-top: 20px;
-    }
-    .dark_title, .white_title {
       font-size: 20px;
     }
   }
