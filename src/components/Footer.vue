da39200ce8b9d353586bac43056ebc387a9045dc
panda <SmelayaPanda@mail.ru>
2018-05-15 03:23:19 +0700

    logo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,11 @@
 <template>
   <footer>
     <el-row class="app_footer" type="flex">
-      <el-col :offset="2" :xs="22" :sm="18" :md="22" :lg="22" :xl="21">
+      <el-col :offset="2" :xs="22" :sm="20" :md="22" :lg="22" :xl="21">
         <el-row type="flex" justify="center" style="flex-wrap: wrap">
           <el-col :xs="12" :sm="12" :md="6" :lg="6" :xl="6" class="footer_block" align="left">
-            <contacts-important v-if="this.$vuetify.breakpoint.name === 'xs' || this.$vuetify.breakpoint.name === 'sm'"/>
+            <contacts-important
+              v-if="this.$vuetify.breakpoint.name === 'xs' || this.$vuetify.breakpoint.name === 'sm'"/>
             <contacts-services v-else/>
           </el-col>
           <el-col :xs="12" :sm="12" :md="6" :lg="6" :xl="6" class="footer_block" align="left">
@@ -14,12 +15,19 @@
           <el-col :xs="24" :sm="12" :md="6" :lg="6" :xl="6" class="footer_block" align="left">
             <contacts-phone id="contacts_phone"/>
           </el-col>
-          <el-col :xs="16" :sm="12" :md="6" :lg="6" :xl="6" class="footer_block" align="left">
+          <el-col :xs="24" :sm="12" :md="6" :lg="6" :xl="6" class="footer_block" align="left">
             <p id="watch_news" align="left">СЛЕДИ ЗА НОВОСТЯМИ</p>
             <contacts-social class="ml-3 mb-4"/>
           </el-col>
         </el-row>
       </el-col>
+      <el-row id="rehigh_logo_wrap" onclick="window.open('https://rehigh.pro/', '_blank')">
+        <el-col :span="24">
+          <img src="@/assets/icons/rehigh/reHIGH.svg" id="rehigh_logo" alt="">
+          <br>
+          <span id="created_by">created by</span>
+        </el-col>
+      </el-row>
     </el-row>
   </footer>
 </template>
@@ -63,6 +71,27 @@ export default {
     font-weight: 600;
     font-size: 13px;
     color: $color-info;
+  }
+
+  #rehigh_logo_wrap {
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    margin-bottom: 25px;
+    margin-top: 5px;
+  }
+
+  #rehigh_logo_wrap:hover {
+    cursor: pointer;
+  }
+
+  #rehigh_logo {
+    height: 18px;
+  }
+
+  #created_by {
+    color: $color-info;
+    font-size: 12px;
   }
 
   @media only screen and (max-width: $xs-screen) {
